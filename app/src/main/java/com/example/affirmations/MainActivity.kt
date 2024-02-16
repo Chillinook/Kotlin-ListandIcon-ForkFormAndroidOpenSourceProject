bc4f37f7d45547eeecacf2ab3d440463658cab42
Chillinook <kwanmygirl@gmail.com>
2024-02-15 21:01:59 -0700

    list+icon change

---
+++ app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -0,0 +1,111 @@
+/*
+ * Copyright (C) 2023 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.example.affirmations
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.layout.rememberLazyNearestItemsRangeState
+import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import com.example.affirmations.data.Datasource
+import com.example.affirmations.model.Affirmation
+import com.example.affirmations.ui.theme.AffirmationsTheme
+
+class MainActivity : ComponentActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            AffirmationsTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    AffirmationsApp()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun AffirmationsApp() {
+    AffirmationsList(aff = Datasource().loadAffirmations() ) ;
+
+
+}
+
+
+@Composable
+fun AffirmationsCard(aff: Affirmation ) {
+    Card(
+        modifier = Modifier
+            .padding(20.dp)
+    ){
+        Image(
+            painter = painterResource(id = aff.imageResourceId),
+            contentDescription = stringResource(id = aff.stringResourceId ),
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .fillMaxWidth()
+//                .padding( 20.dp)
+//                .size( 200.dp)
+        )
+        Text(
+            text = stringResource(id = aff.stringResourceId ),
+            textAlign = TextAlign.Center
+
+        )
+    }
+}
+
+@Composable
+fun AffirmationsList( aff: List<Affirmation> ) {
+    LazyColumn {
+        items (aff){
+            affir -> AffirmationsCard( affir );
+        }
+
+//        AffirmationsCard() ;
+//        AffirmationsCard() ;
+//        AffirmationsCard() ;
+    }
+
+}
+
+
+
+
+
